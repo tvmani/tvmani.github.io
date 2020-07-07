add901ec26e34e0390c8e629eab0e98a37b4cf8e
Peace & Patience <peace@patience.com>
2020-07-07 17:35:31 +0800

    move the focus to answer on name submitted

--- scripts/math_operation.js
+++ scripts/math_operation.js
@@ -44,6 +44,8 @@ function registerUser(studentId) {
     studentDetails.studentId.toLowerCase(),
     JSON.stringify(studentDetails)
   );
+
+  document.getElementById("answer").focus();
 }
 
 function yourNameKeyboardHandler() {
