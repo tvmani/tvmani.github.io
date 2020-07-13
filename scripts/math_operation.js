d18941b2f78efa1e8ff3e27528dabcd23b965751
Mohan Narayanaswamy <mohanmca@gmail.com>
2020-07-13 20:00:47 +0800

    math bundle

--- scripts/math_operation.js
+++ scripts/math_operation.js
@@ -72,6 +72,12 @@ window.addEventListener('load', (_event) => {
 export { uiTools as ui };
 
 export function scoreMark(question) {
+  if (document.getElementById('answer').value && document.getElementById('answer').value.length > 1) {
+    //do nothing..
+  } else {
+    console.log('enter usage without input!');
+    return false;
+  }
   if (Evaluator.evaluateQuestion(question)) {
     totalCorrect++;
   } else {
