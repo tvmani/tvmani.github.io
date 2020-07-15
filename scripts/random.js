ddadcde62c4a073f435a1c107a15477a14509c26
Mohan Narayanaswamy <mohanmca@gmail.com>
2020-07-15 18:58:12 +0800

    Package

--- scripts/random.js
+++ scripts/random.js
@@ -1,3 +1,6 @@
+//import { shuffle } from 'lodash/fp/shuffle';
+const shuffle = require('lodash/fp/shuffle');
+
 export default class Random {
   /**
    * Credit : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -10,8 +13,12 @@ export default class Random {
     return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
   }
 
+  suffle
+
   static getRandomIntInclusiveWithExceptions(min, max, ...excludes) {
-    const result = this.getRandomIntInclusive(min, max);
-    return excludes.indexOf(result) == -1 ? result : this.getRandomIntInclusiveWithExceptions(min, max, excludes);
+    let generated = Array.from(Array(max - min).keys()).map(i => i + min).filter(i => excludes.indexOf(i)!=-1);
+    let unique = new Set(generated);
+    excludes.forEach(k => unique.delete(k));
+    return shuffle(Array.from(unique))[0];
   }
 }
--- scripts/random.test.js
+++ scripts/random.test.js
@@ -1,34 +1,57 @@
-const { Random } = require('./random');
+import Random from "./random";
+import * as fc from "fast-check";
 
-const rand = new Random();
+// expect.extend({
+//   toBeWithinRange(received, floor, ceiling) {
+//     const pass = received >= floor && received <= ceiling;
+//     if (pass) {
+//       return {
+//         message: () =>
+//           `expected ${received} not to be within range ${floor} - ${ceiling}`,
+//         pass: true,
+//       };
+//     }
+//     return {
+//       message: () =>
+//         `expected ${received} to be within range ${floor} - ${ceiling}`,
+//       pass: false,
+//     };
+//   },
+// });
 
-expect.extend({
-  toBeWithinRange(received, floor, ceiling) {
-    const pass = received >= floor && received <= ceiling;
-    if (pass) {
-      return {
-        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
-        pass: true,
-      };
-    }
-    return {
-      message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
-      pass: false,
-    };
-  },
-});
+// test("Random number should be within boundry", () => {
+//   expect(Random.getRandomIntInclusive(3, 7)).toBeWithinRange(3, 7);
+// });
 
-test('Random number should be within boundry', () => {
-  expect(Random.getRandomIntInclusive(3, 7)).toBeWithinRange(3, 7);
-});
+// test("Random number should be within boundry", () => {
+//   const answer = Random.getRandomIntInclusiveWithExceptions(3, 7, [6]);
+//   expect(answer).toBeWithinRange(3, 7);
+//   expect(answer).not.toBe(6);
+// });
 
-test('Random number should be within boundry', () => {
-  const answer = Random.getRandomIntInclusiveWithExceptions(3, 7, [6]);
-  expect(answer).toBeWithinRange(3, 7);
-  expect(answer).not.toBe(6);
-});
+// test("Random number should be within boundry", () => {
+//   const answer = Random.getRandomIntInclusiveWithExceptions(2, 17, [4, 5]);
+//   expect(answer).toBeWithinRange(2, 17);
+// });
 
-test('Random number should be within boundry', () => {
-  const answer = Random.getRandomIntInclusiveWithExceptions(2, 17, [4, 5]);
-  expect(answer).toBeWithinRange(2, 17);
+// test("Random number should not be out of boundry", () => {
+//   fc.assert(
+//     fc.property(fc.integer(1,12), fc.integer(1,12), (a, b) => {
+//       const answer = Random.getRandomIntInclusiveWithExceptions(a, b, [10]);
+//       expect(answer).not.toBe(100);
+
+//     })
+//   );
+// });
+
+test("Random number should be within boundry", () => {
+  fc.assert(
+    fc.property( fc.tuple(fc.integer(1, 12), fc.integer(1, 12)), (data) => {
+      const inputs = [data[0], data[1]];
+      inputs.sort((x, y) => x - y);
+      const answer = Random.getRandomIntInclusiveWithExceptions(inputs[0], inputs[1], [10, 6]);
+      console.log(`Inputs: ${inputs}, Generate number is ${answer}`);
+      expect(answer).not.toBe(10);
+    })
+  );
 });
