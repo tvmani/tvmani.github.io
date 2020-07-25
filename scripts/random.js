cd12cdfc7dbf01d43a7e5802fc54590ea929b9f8
Mohan Narayanaswamy <mohanmca@gmail.com>
2020-07-25 10:15:59 +0800

    simplify random exlcudes

--- scripts/random.js
+++ scripts/random.js
@@ -24,15 +24,12 @@ export default class Random {
   
 
   static getRandomIntInclusiveWithExceptions(min, max, excludes) {
-    const _excludes = excludes;
-
     // eslint-disable-next-line arrow-parens
     const sequence = Array.from(Array(max - min).keys()).map(i => i + min).map(i => ''+i)
-        .filter( i => _excludes.indexOf(i) < 0 ).filter( i => _excludes.indexOf(+i) < 0 ) 
-        .filter( i => _excludes.indexOf(i) < 0 ).filter( i => _excludes.indexOf(+i) < 0 );
-    const generated = difference(sequence, _excludes);
+        .filter( i => excludes.indexOf(i) < 0 ).filter( i => excludes.indexOf(+i) < 0 );
+    const generated = difference(sequence, excludes);
     const answer = shuffle(Array.from(generated))[0];
-    console.log(`Excludes: ${_excludes}, (Min, max): (${min}, ${max}), Sequence: ${sequence},  Generated: ${generated}, Answer - ${answer}`);
+    console.log(`Excludes: ${excludes}, (Min, max): (${min}, ${max}), Sequence: ${sequence},  Generated: ${generated}, Answer - ${answer}`);
     return parseInt(answer, 10);
   }
 }
