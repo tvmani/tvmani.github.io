9b07a3c96653e086d53c5501545ee181793d1187
Peace & Patience <peace@patience.com>
2020-08-19 10:23:27 +0800

    pyxl library exploring

--- tamil_biology/002_openpyxl.py
+++ tamil_biology/002_openpyxl.py
@@ -1,4 +1,20 @@
+from openpyxl import Workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl import load_workbook
 
 wb = load_workbook(filename = r"E:\Resources\RangeBook.xlsx")
 
+# my_range = wb.defined_names['Fruits_Price']
+
+sheetOne = wb['One']
+
+print(sheetOne.tables.items())
+
+tblFruitsPrice = sheetOne.tables.get("Fruits_Price")
+
+print(tblFruitsPrice)
+
+# for table in sheetOne.tables.values():
+# #     print(table)
+# for column in table.:
+#     column.name = value
---
+++ tamil_biology/005_openpyxl_read_wb.py
@@ -0,0 +1,7 @@
+from openpyxl import load_workbook
+
+wb = load_workbook(filename = 'empty_book.xlsx')
+
+sheet_ranges = wb['range names']
+
+print(sheet_ranges['WA18'].value)
