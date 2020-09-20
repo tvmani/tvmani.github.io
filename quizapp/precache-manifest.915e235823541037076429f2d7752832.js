30f3a261b4777409abee7246b89566ad5fb5510c
manikandan <tv.mani@gmail.com>
2020-09-20 17:02:59 +0800

    quiz app - first version

---
+++ quizapp/precache-manifest.915e235823541037076429f2d7752832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe68f82bd746766dc4c43fad3aca7c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95470ccd2db034821f2a",
+    "url": "/static/css/main.bab91c15.chunk.css"
+  },
+  {
+    "revision": "491b0db76983ce28d6cc",
+    "url": "/static/js/2.a136da81.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "/static/js/2.a136da81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95470ccd2db034821f2a",
+    "url": "/static/js/main.e360b840.chunk.js"
+  },
+  {
+    "revision": "6beec426529fbefc8a6d",
+    "url": "/static/js/runtime-main.8bc3e47f.js"
+  }
+]);
