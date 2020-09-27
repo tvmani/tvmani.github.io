a190fc39a1e83f4b561f1457c103de87c0b67cd7
manikandan <tv.mani@gmail.com>
2020-09-28 07:56:22 +0800

    quiz 12_tm_botany added

---
+++ quiz/precache-manifest.f59d89f489f31c60e875d56ceec41e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7b78d361e84db900d71c08a07bbb6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617af84a2904053ddb22",
+    "url": "/static/css/main.bab91c15.chunk.css"
+  },
+  {
+    "revision": "491b0db76983ce28d6cc",
+    "url": "/static/js/2.a136da81.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "/static/js/2.a136da81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617af84a2904053ddb22",
+    "url": "/static/js/main.18983a1e.chunk.js"
+  },
+  {
+    "revision": "6beec426529fbefc8a6d",
+    "url": "/static/js/runtime-main.8bc3e47f.js"
+  }
+]);
