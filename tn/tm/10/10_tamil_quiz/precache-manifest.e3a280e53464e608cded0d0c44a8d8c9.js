2156d2bb783c248b664634a7a90bd27ca9320fea
manikandan <tv.mani@gmail.com>
2020-10-02 16:37:15 +0800

    10_Tamil_L01_Q1 added

---
+++ tn/tm/10/10_tamil_quiz/precache-manifest.e3a280e53464e608cded0d0c44a8d8c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bdac4dfb1d53843b2290861740b050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ea862ff4d2b8de328d",
+    "url": "/static/css/main.bab91c15.chunk.css"
+  },
+  {
+    "revision": "491b0db76983ce28d6cc",
+    "url": "/static/js/2.a136da81.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "/static/js/2.a136da81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ea862ff4d2b8de328d",
+    "url": "/static/js/main.a2244081.chunk.js"
+  },
+  {
+    "revision": "6beec426529fbefc8a6d",
+    "url": "/static/js/runtime-main.8bc3e47f.js"
+  }
+]);
